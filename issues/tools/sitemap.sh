e2c8918a4925a29a9afc54c6ce0562b2e3488547
prx <prx@ybad.name>
2021-09-29 21:28:39 +0200

    fix sitemap header

--- issues/tools/sitemap.sh
+++ issues/tools/sitemap.sh
@@ -13,14 +13,9 @@ test $# -eq 2 || usage
 ndd="$1"
 
 # sitemap header
-printf '%s\n' \
 cat <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
-<urlset
-xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
-xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 EOF
 
 # list all pages in site_directory and print last mod time
